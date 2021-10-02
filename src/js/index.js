c0893f37b74e5dc0d6c3394351a6353abb862ef3
Scott <sstinchc@gmail.com>
2021-10-02 14:48:43 -0400

    testing

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 window.onload = () => {                       navigator.geolocation.getCurrentPosition((position) => {
-    document.querySelector('a-box').setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
+   document.querySelector('a-box').setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude} -  .00010;`)
   });
  }
